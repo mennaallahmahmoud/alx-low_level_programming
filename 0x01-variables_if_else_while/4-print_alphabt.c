42f89fcade43886875db8655abadb22baac0769d
Menna Allah Mahmoud <mmenna.236@gmail.com>
2023-07-26 18:43:57 -0700

    print alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 /**
- * main - Prog Entry point
- * Return: Always 0 (Success)
- */
+*main - Prog Entry point
+*Return: Always 0(Success)
+*/
 int main(void)
 {
-	char c;
-        c = 'a';
-	while
-		(c <= 'z') {
-			if ((c != 'q' && c != 'e') && c <= 'z')
-				putchar(c);
-			c++;
-		}
-	putchar('\n');
-	return (0);
+char c;
+c = 'a';
+while
+(c <= 'z') {
+if ((c != 'q' && c != 'e') && c <= 'z')
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
 }	
